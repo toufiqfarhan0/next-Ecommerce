37f439bf494da7e3885daf4b1a7ed9a9e0b1ad3c
Farhan <144435177+toufiqfarhan0@users.noreply.github.com>
2024-08-18 16:49:32 +0530

    fetching data from stripe

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
 import Image from "next/image";
+import Stripe from "stripe";
+import Product from "./components/Product";
 
-export default function Home() {
+const getProducts = async () => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
+    apiVersion: "2022-11-15",
+  });
+  const products = await stripe.products.list();
+
+  const productsWithPrice = await Promise.all(
+    products.data.map(async (product) => {
+      const prices = await stripe.prices.list({ product: product.id });
+      return {
+        id: product.id,
+        name: product.name,
+        price: prices.data[0].unit_amount,
+        image: product.images[0],
+        currency: prices.data[0].currency,
+      };
+    })
+  );
+  return productsWithPrice;
+};
+
+export default async function Home() {
+  const products = await getProducts();
+
   return (
-   <div>
-    <h1>helko next</h1>
-   </div>
+    <div>
+      {products.map((product) => (
+        <Product {...product} />
+      ))}
+    </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,16 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',  // Allow any path on this hostname
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "files.stripe.com",
+        port: "",
+        pathname: "/**",
       },
       // Your other allowed hostnames (if any)
     ],
